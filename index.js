fe1d04ccad68e8ed6151280dd4ffc38d0681f1fd
KlNGU <brandynvay@yahoo.com>
2019-06-25 22:08:16 -0700

    testing addition and subtraction

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ console.log(greet('JOHN'));
 console.log(math.add(1,3)); // 4
 console.log(math.subtract(1,3)); // -2
 console.log(math.multiply(2)); // 8
-console.log(math.divide(20, 8)) // 5, 2
+console.log(math.divide(20, 8)); // 5, 2
