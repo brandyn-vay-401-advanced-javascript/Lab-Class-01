b5a643e2d34fd8724ac44a711cf3626565f3cd2f
Brandyn Vay <brandynvay@protonmail.com>
2019-08-18 19:31:12 -0700

    done

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,4 +1,29 @@
 'use strict';
 
-const greeting = require ('../lib/greet.js');
-const faker = require ('faker');
+const greeting = require('../lib/greet.js');
+const faker = require('faker');
+
+describe('Greet Test', () => {
+
+  it('greets a random person appropriately', () => {
+    let user = faker.random.word();
+    expect(greeting.greetUser(user)).toEqual(`What's up, ${user}!!`);
+  });
+
+  it('greets a random person the way they want', () => {
+    let user = faker.name.firstName();
+    let greetThem = faker.lorem.words();
+    expect(greeting.greetUser(user, greetThem)).toEqual(`${greetThem}, ${user}!!`);
+  });
+
+  it('returns null when you don\'t pass a string', () => {
+    let notName = 4;
+    expect(greeting.greetUser(notName)).toEqual(null);
+  });
+
+  it('hello worlds', () => {
+    let user = 'world';
+    let greetThem = 'Hello';
+    expect(greeting.greetEm(user, greetThem)).toEqual(`${greetThem}, ${user}!!`);
+  });
+});
